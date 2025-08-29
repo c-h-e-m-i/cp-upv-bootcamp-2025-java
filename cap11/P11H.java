d5c301b0500f2d8f4952b25d5200a21802e2d21e
chemi <132817327+c-h-e-m-i@users.noreply.github.com>
2025-08-29 23:56:12 +0200

    Capítulo 11 (problemas H, I)

---
+++ cap11/P11H.java
@@ -0,0 +1,273 @@
+import java.io.*;
+import java.util.*;
+
+public class P11H {
+    static Lector sc;
+    static StringBuilder output;
+    static final int SIN = (int) Math.pow(10, 8);
+
+    public static void main(String[] args) throws IOException {
+        // Inicializamos entrada y salida:
+        inicializar();
+
+        // Resolvemos el problema:
+        solve();
+    }
+
+    public static void solve() throws IOException {
+        // Leemos las coordenadas de Arenópolis:
+        Punto aren = new Punto(sc.nextInt(), sc.nextInt());
+
+        /*
+         * La idea de este problema es similar a la del anterior: los puntos donde la
+         * dirección hacia la singularidad valga IND estarán en una frontera ("rayo")
+         * entre 2 direcciones. El punto de corte entre 2 rayos será la singularidad,
+         * así que realizaremos búsqueda binaria sobre uno de los ejes cardinales para
+         * dar con ellos.
+         */
+        buscar(aren);
+    }
+
+    static void preguntar(Punto p) {
+        System.out.printf("? %d %d%n", p.x, p.y);
+        System.out.flush();
+    }
+
+    static void confirmar(Punto p) {
+        System.out.printf("! %d %d%n", p.x, p.y);
+        System.out.flush();
+    }
+
+    static void buscar(Punto aren) throws IOException {
+        preguntar(aren);
+
+        Punto rayo_A = null, rayo_B = null;
+        boolean norte_u_oeste = false;
+
+        /*
+         * Si la dirección es norte o sur, exploro el eje X; si es este u oeste, el eje
+         * Y, y si estoy encima de un rayo, me muevo una posición a la derecha y vuelvo
+         * a preguntar:
+         */
+        switch (sc.next().charAt(0)) {
+            case 'N':
+                norte_u_oeste = true;
+            case 'S':
+                // ini = izquierda, fin = derecha
+                rayo_A = busquedaA(new Punto(-SIN * 3, aren.y), new Punto(aren.x - 1, aren.y), true); // Izquierdo
+                rayo_B = busquedaB(new Punto(aren.x + 1, aren.y), new Punto(SIN * 3, aren.y), true); // Derecho
+                break;
+            case 'O':
+                norte_u_oeste = true;
+            case 'E':
+                // ini = abajo, fin = arriba
+                rayo_A = busquedaA(new Punto(aren.x, aren.y + 1), new Punto(aren.x, SIN * 3), false); // Arriba
+                rayo_B = busquedaB(new Punto(aren.x, -SIN * 3), new Punto(aren.x, aren.y - 1), false); // Abajo
+                break;
+            case 'I':
+                // Si ya estoy colocado sobre un rayo, me muevo hacia un lado y vuelvo a buscar;
+                aren.x += 1;
+                buscar(aren);
+                return;
+        }
+
+        // Hallo el punto de corte entre los 2 rayos, que será la singularidad:
+        Punto corte = corte(rayo_A, rayo_B, norte_u_oeste);
+        confirmar(corte);
+    }
+
+    static Punto busquedaA(Punto ini, Punto fin, boolean eje) throws IOException {
+        Punto medio = ini.mas(fin).entre(2);
+        preguntar(medio);
+        char dir = sc.next().charAt(0);
+
+        if (dir == 'I') {
+            return medio;
+        }
+
+        // Hago búsqueda binaria según la dirección donde se encuentre la singularidad:
+        if (eje) { // Si eje == true --> eje X, si eje == false --> eje Y
+            if (dir == 'E')
+                return busquedaA(new Punto(medio.x + 1, medio.y), fin, eje);
+            else
+                return busquedaA(ini, new Punto(medio.x - 1, medio.y), eje);
+        } else {
+            if (dir == 'S')
+                return busquedaA(ini, new Punto(medio.x, medio.y - 1), eje);
+            else
+                return busquedaA(new Punto(medio.x, medio.y + 1), fin, eje);
+        }
+    }
+
+    static Punto busquedaB(Punto ini, Punto fin, boolean eje) throws IOException {
+        Punto medio = ini.mas(fin).entre(2);
+        preguntar(medio);
+        char dir = sc.next().charAt(0);
+
+        if (dir == 'I') {
+            return medio;
+        }
+
+        // Hago búsqueda binaria según la dirección donde se encuentre la singularidad:
+        if (eje) { // Si eje == true --> eje X, si eje == false --> eje Y
+            if (dir == 'O')
+                return busquedaB(ini, new Punto(medio.x - 1, medio.y), eje);
+            else
+                return busquedaB(new Punto(medio.x + 1, medio.y), fin, eje);
+        } else {
+            if (dir == 'N')
+                return busquedaB(new Punto(medio.x, medio.y + 1), fin, eje);
+            else
+                return busquedaB(ini, new Punto(medio.x, medio.y - 1), eje);
+        }
+    }
+
+    static Punto corte(Punto rayo_A, Punto rayo_B, boolean norte_u_oeste) {
+        /*
+         * Nuestros rayos son rectas definidas por un punto y un vector director. Si
+         * hemos explorado el eje Y estando al este de la singularidad, el rayo de
+         * arriba tendrá de pendiente -1 y el otro 1, y viceversa estando al oeste. Lo
+         * mismo con el eje X: si estábamos al norte de al singularidad, el rayo
+         * izquierdo tendrá de pendiente -1 y el otro 1, y viceversa si estábamos al
+         * sur. Sabiendo esto, para encontrar el punto de corte entre 2 rayos tendremos
+         * que igualar sus ecuaciones:
+         * 
+         * rayo_A + (1, -1) * a = rayo_B + (1, 1) * b {
+         * 
+         * rayo_A.x + a = rayo_B.x + b
+         * rayo_A.y - a = rayo_B.y + b
+         * 
+         * }
+         * 
+         * Sumamos ambas igualdades...
+         * 
+         * rayo_A.x + rayo_A.y = rayo_B.x + rayo_B.y + 2b
+         * 
+         * Despejamos la b...
+         * 
+         * b = [(rayo_A.x + rayo_A.y) - (rayo_B.x + rayo_B.y)] / 2
+         * 
+         * Para encontrar el punto, sustituimos la b en:
+         * 
+         * rayo_B + (1, 1) * b = rayo_B + (b, b)
+         */
+        if (norte_u_oeste) {
+            Punto aux = rayo_A;
+            rayo_A = rayo_B;
+            rayo_B = aux;
+        }
+
+        int b = ((rayo_A.x + rayo_A.y) - (rayo_B.x + rayo_B.y)) / 2;
+        return new Punto(rayo_B.x + b, rayo_B.y + b);
+    }
+
+    // -------- CLASES Y MÉTODOS AUXILIARES --------
+    public static void inicializar(int cap, char prob) throws IOException {
+        File f = new File(String.format("inputs/%02d/%C.txt", cap, prob));
+        sc = new Lector();
+        sc.leerArchivo(f);
+        output = new StringBuilder();
+    }
+
+    public static void inicializar() {
+        sc = new Lector();
+        sc.leerStd();
+        output = new StringBuilder();
+    }
+
+    @SuppressWarnings("unused")
+    private static class Lector {
+        BufferedReader br;
+        StringTokenizer st;
+
+        Lector() {
+            br = null;
+            st = new StringTokenizer("");
+        }
+
+        void leerArchivo(File f) throws IOException {
+            br = new BufferedReader(new FileReader(f));
+        }
+
+        void leerStd() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        void cerrar() throws IOException {
+            br.close();
+        }
+
+        String next() throws IOException {
+            if (!st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() throws IOException {
+            if (!st.hasMoreTokens()) {
+                return br.readLine();
+            } else {
+                StringBuilder resto = new StringBuilder();
+                while (st.hasMoreTokens()) {
+                    resto.append(st.nextToken()).append(" ");
+                }
+                return resto.toString().trim();
+            }
+        }
+
+        byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        boolean nextBoolean() throws IOException {
+            return Boolean.parseBoolean(next());
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private static class Punto {
+        int x, y;
+
+        Punto(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        Punto menos(Punto p) {
+            return new Punto(this.x - p.x, this.y - p.y);
+        }
+
+        Punto mas(Punto p) {
+            return new Punto(this.x + p.x, this.y + p.y);
+        }
+
+        Punto entre(int d) {
+            return new Punto(this.x / d, this.y / d);
+        }
+
+        double mod() {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        }
+    }
+}
---
+++ cap11/P11I.java
@@ -0,0 +1,215 @@
+import java.io.*;
+import java.util.*;
+
+public class P11I {
+    static Lector sc;
+    static StringBuilder output;
+
+    public static void main(String[] args) throws IOException {
+        // Inicializamos entrada y salida:
+        inicializar(11, 'I');
+
+        // Resolvemos el problema:
+        solve();
+    }
+
+    public static void solve() throws IOException {
+        /*
+         * Leemos la puntos de entrada y calculamos el de mínima Y (si hay 2 con la
+         * misma Y, de esos 2 cogemos el que tenga menor X). Los guardaré en una
+         * LinkedList para poder borrar fácilmente dicho punto después de encontrarlo:
+         */
+        int p = sc.nextInt(), min_ind = -1;
+
+        if (p <= 2) {
+            System.out.printf("0.0000");
+            return;
+        }
+
+        Punto min_punto = new Punto(Double.MAX_VALUE, Double.MAX_VALUE);
+        LinkedList<Punto> puntos = new LinkedList<>();
+        for (int i = 0; i < p; i++) {
+            Punto leido = new Punto(sc.nextDouble(), sc.nextDouble());
+            puntos.add(leido);
+
+            if ((leido.y < min_punto.y) || (Math.abs(min_punto.y - leido.y) < 1e-9 && leido.x < min_punto.x)) {
+                min_punto = leido;
+                min_ind = i;
+            }
+        }
+        puntos.remove(min_ind);
+
+        /*
+         * Ordenamos la lista resultante en función del ángulo polar (ángulo antihorario
+         * respecto del eje X entre 0° y 360°) que forma cada punto con 'min_punto'. Los
+         * 2 de ángulo menor serán los siguientes en nuestra envolvente convexa.
+         * 
+         * Para hallar dichos ángulos podemos emplear un truco muy ingenioso: la
+         * tangente. La tangente no es más la pendiente de la hipotenusa del triángulo
+         * formado por el seno y el coseno de un ángulo. Pasa que dicha tangente va
+         * tomando valores positivos y negativos de manera periódica, así que usaremos
+         * la función 'Math.atan2()', que devuelve la arcotangente de un ángulo polar:
+         */
+        Punto aux = min_punto; // Variable efectivamente final para que no nos dé problemas meterla en la
+                               // expresión lambda
+        Collections.sort(puntos, (a, b) -> {
+            Punto dif_a = a.menos(aux), dif_b = b.menos(aux);
+            double angulo_A = Math.atan2(dif_a.y, dif_a.x), angulo_B = Math.atan2(dif_b.y, dif_b.x);
+            if (Math.abs(angulo_A - angulo_B) > 1e-9)
+                return angulo_A < angulo_B ? -1 : 1;
+            else
+                return dif_a.mod2() < dif_b.mod2() ? -1 : 1;
+        });
+
+        // Una vez ordenada la lista, guardo los 2 primeros puntos en la pila donde
+        // almacenaremos la envolvente:
+        Stack<Punto> hull = new Stack<>();
+        hull.push(min_punto);
+        hull.push(puntos.poll());
+
+        /*
+         * Mientras queden puntos por analizar, los voy guardando en 'sig' y compruebo
+         * que el vector que forman con el último punto de la envolvente actual ('act')
+         * represente un giro antihorio respecto del vector formado por 'ant' y 'act':
+         */
+        while (!puntos.isEmpty()) {
+            Punto sig = puntos.poll();
+
+            while (hull.size() >= 2) {
+                Punto act = hull.pop(), ant = hull.peek();
+
+                if (convexo(act.menos(ant), sig.menos(act))) {
+                    hull.push(act);
+                    break;
+                }
+            }
+
+            hull.push(sig);
+        }
+
+        // Finalmente emplearé el teorema del cordel para calcular el área:
+        if (hull.size() < 3)
+            System.out.printf("0.0000");
+        else
+            System.out.printf(Locale.US, "%.4f", cordel(hull));
+    }
+
+    static boolean convexo(Punto vector_A, Punto vector_B) {
+        return vector_A.pvec(vector_B) > 0;
+    }
+
+    static double cordel(List<Punto> hull) {
+        double suma = 0;
+        for (int i = 0; i < hull.size() - 1; i++) {
+            suma += (hull.get(i).x * hull.get(i + 1).y - hull.get(i).y * hull.get(i + 1).x);
+        }
+        suma += hull.get(hull.size() - 1).x * hull.get(0).y - hull.get(hull.size() - 1).y * hull.get(0).x;
+        return 0.5 * Math.abs(suma);
+    }
+
+    // -------- CLASES Y MÉTODOS AUXILIARES --------
+    public static void inicializar(int cap, char prob) throws IOException {
+        File f = new File(String.format("inputs/%02d/%C.txt", cap, prob));
+        sc = new Lector();
+        sc.leerArchivo(f);
+        output = new StringBuilder();
+    }
+
+    public static void inicializar() {
+        sc = new Lector();
+        sc.leerStd();
+        output = new StringBuilder();
+    }
+
+    @SuppressWarnings("unused")
+    private static class Lector {
+        BufferedReader br;
+        StringTokenizer st;
+
+        Lector() {
+            br = null;
+            st = new StringTokenizer("");
+        }
+
+        void leerArchivo(File f) throws IOException {
+            br = new BufferedReader(new FileReader(f));
+        }
+
+        void leerStd() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        void cerrar() throws IOException {
+            br.close();
+        }
+
+        String next() throws IOException {
+            if (!st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() throws IOException {
+            if (!st.hasMoreTokens()) {
+                return br.readLine();
+            } else {
+                StringBuilder resto = new StringBuilder();
+                while (st.hasMoreTokens()) {
+                    resto.append(st.nextToken()).append(" ");
+                }
+                return resto.toString().trim();
+            }
+        }
+
+        byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        boolean nextBoolean() throws IOException {
+            return Boolean.parseBoolean(next());
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private static class Punto {
+        double x, y;
+
+        Punto(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        Punto menos(Punto p) {
+            return new Punto(this.x - p.x, this.y - p.y);
+        }
+
+        double pvec(Punto p) {
+            return this.x * p.y - this.y * p.x;
+        }
+
+        double mod2() {
+            return this.x * this.x + this.y * this.y;
+        }
+    }
+}
